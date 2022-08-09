439c376d5b83dabe7e3f44b27a782ce19014043d
durnf <dreadlordfluffybunny@gmail.com>
2022-08-09 20:29:40 -0230

    Added win conditon and hangman ascii art

--- src/WordActions.java
+++
@@ -1,9 +0,0 @@
-public class WordActions {
-    public static boolean checkWord(String word, String guess) {
-        return word.equals(guess);
-    }
-
-    public static boolean checkLetter(String word, String guess) {
-        return word.contains(guess);
-    }
-}
